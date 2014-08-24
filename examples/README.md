3a1a44900908ee57405d328e37d0a209558a4765
Dirk Neumann <dirk@caltech.edu>
2014-08-24 15:23:28 -0700

    examples updated.

---
+++ examples/README.md
@@ -0,0 +1,6 @@
+Training Word2Vec with a small Wiki Dump:
+
+```shell
+curl http://cs.fit.edu/~mmahoney/compression/enwik8.zip -o enwik8.zip
+unzip enwik8.zip
+```
--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(name='deepdist',
       version='0.1',
-      description='Lighting-Fast Deep Learning on Spark',
-      url='http://github.com/dirkneumann/deepdist',
+      description='Lighting-Fast Deep Learning on Spark | asynchronous stochastic gradient descent',
+      url='http://deepdist.com',
       author='Dirk Neumann',
       author_email='dirk@caltech.edu',
       packages=['deepdist'],
